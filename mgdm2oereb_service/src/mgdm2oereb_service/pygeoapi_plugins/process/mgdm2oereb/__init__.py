4ad70843a741f95078265894920a81b0a5ef5f79
Clemens Rudert <crud3_rt@posteo.ch>
2023-07-04 18:16:32 +0200

    fix params

--- mgdm2oereb_service/src/mgdm2oereb_service/pygeoapi_plugins/process/mgdm2oereb/__init__.py
+++ mgdm2oereb_service/src/mgdm2oereb_service/pygeoapi_plugins/process/mgdm2oereb/__init__.py
@@ -250,8 +250,7 @@ class Mgdm2OerebTransformatorBase(BaseProcessor):
         """
 
         files = {
-            'file': (result_xtf_file_name, xtf_content),
-            'allObjectsAccessible': 'true' if all_objects_accessible else 'false'
+            'file': (result_xtf_file_name, xtf_content)
         }
         if all_objects_accessible:
             query_string = '?allObjectsAccessible=true'
